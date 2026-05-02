3b307ff369d4b4497025f3f1443a30ca21b072f0
hmmmmnnn... <leap.omit-0d@icloud.com>
2026-05-02 14:52:34 +0900

    Update database connection credentials in DBManager

--- Back/DB/DBManager.java
+++ Back/DB/DBManager.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBManager {
-    private static final String url = "jdbc:oracle:thin:@htaeky.iptime.org:8000/FREEPDB1";
-    private static final String user = "user1";
-    private static final String pw = "00000000";   
+    private static final String url = "jdbc:oracle:thin:@...";
+    private static final String user = "";
+    private static final String pw = "";   
 
     static{
         try {
